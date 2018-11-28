2b23df80123cc79c2e21411196422ef3e52d0351
Sean Keckler <seankeckler@gmail.com>
2018-11-28 15:34:11 -0500

    First Commit

--- CarDealership/Controllers/ValuesController.cs
+++ CarDealership/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarDealership.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,8 +8,10 @@ using System.Web.Http;
 
 namespace CarDealership.Controllers
 {
+    
     public class ValuesController : ApiController
     {
+
         // GET api/values
         public IEnumerable<string> Get()
         {
---
+++ CarDealership/Models/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CarDealership.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        
+        public Car()
+        {
+
+        }
+
+        public Car(string Make, string Model, int Year, string Color)
+        {
+            this.Make = Make;
+            this.Model = Model;
+            this.Year = Year;
+            this.Color = Color;
+        }
+    }
+
+    public class CarContext : DbContext
+    {
+        //DbSet<Car> Cars { get; set; }
+
+        public System.Data.Entity.DbSet<CarDealership.Models.Car> Cars { get; set; }
+    }
+}
